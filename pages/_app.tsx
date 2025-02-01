d8ed40b9f44628cfb7409a0e43e9bb733fdd848f
Viqtor <viqto0r@gmail.com>
2025-02-01 18:32:55 +0300

    feat: добавлен Head

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,17 @@
 import { AppProps } from 'next/dist/next-server/lib/router/router'
 import '../styles/globals.css'
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>My top - наш лучший топ</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
