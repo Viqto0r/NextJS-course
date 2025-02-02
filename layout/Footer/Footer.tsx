5bd8c940bfb3a187c01a21c2c152cc7bf1abe8f7
Viqtor <viqto0r@gmail.com>
2025-02-02 15:38:58 +0300

    feat: добавлен компонент Footer и библиотека date-fns

--- layout/Footer/Footer.tsx
+++ layout/Footer/Footer.tsx
@@ -1,7 +1,19 @@
 import { FC } from 'react'
+import cn from 'classnames'
 import { IFooterProps } from './Footer.props'
 import styles from './Footer.module.css'
+import { format } from 'date-fns'
 
-export const Footer: FC<IFooterProps> = ({ ...props }) => {
-  return <footer {...props}>Footer</footer>
+export const Footer: FC<IFooterProps> = ({ className, ...props }) => {
+  return (
+    <footer className={cn(styles.footer, className)} {...props}>
+      <div>OwlTop © 2020 - {format(new Date(), 'yyyy')} Все права защищены</div>
+      <a href="#" target="_blank">
+        Пользовательское соглашение
+      </a>
+      <a href="#" target="_blank">
+        Политика конфиденциальности
+      </a>
+    </footer>
+  )
 }
