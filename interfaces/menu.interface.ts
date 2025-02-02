c8d1069a25616b4bc7ab014803eada8c3cca96d9
Viqtor <viqto0r@gmail.com>
2025-02-02 18:26:55 +0300

    feat: добавлена страница курсов

---
+++ interfaces/menu.interface.ts
@@ -0,0 +1,23 @@
+import { ETopLevelCategory } from './page.interface'
+
+export interface IPageItem {
+  alias: string
+  title: string
+  _id: string
+  category: string
+}
+
+export interface IMenuItem {
+  _id: {
+    secondCategory: string
+  }
+  isOpened?: boolean
+  pages: IPageItem[]
+}
+
+export interface FirstLevelMenuItem {
+  route: string
+  name: string
+  icon: JSX.Element
+  id: ETopLevelCategory
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,32 @@
 import { useState } from 'react'
 import { Button, H, P, Rating, Tag } from '../components'
 import { withLayout } from '../layout/Layout'
+import { GetStaticProps } from 'next'
+import axios from 'axios'
+import { IMenuItem } from '../interfaces/menu.interface'
 
-function Home(): JSX.Element {
+interface IHomeProps extends Record<string, unknown> {
+  menu: IMenuItem[]
+  firstCategory: number
+}
+
+export const getStaticProps: GetStaticProps<IHomeProps> = async () => {
+  const firstCategory = 0
+
+  const { data: menu } = await axios.post<IMenuItem[]>(
+    process.env.NEXT_PUBLIC_DOMAIN + '/api/top-page/find',
+    { firstCategory }
+  )
+
+  return {
+    props: {
+      menu,
+      firstCategory,
+    },
+  }
+}
+
+function Home({ menu }: IHomeProps): JSX.Element {
   const [rating, setRating] = useState(4)
 
   return (
@@ -26,6 +50,11 @@ function Home(): JSX.Element {
       </Tag>
       <Tag color="primary">Primary</Tag>
       <Rating rating={rating} isEditable setRating={setRating} />
+      <ul>
+        {menu.map((m) => (
+          <li key={m._id.secondCategory}>{m._id.secondCategory}</li>
+        ))}
+      </ul>
     </>
   )
 }
