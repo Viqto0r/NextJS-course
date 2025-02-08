1639016565513e7c30890dede2404da69ef1aeb7
Viqtor <viqto0r@gmail.com>
2025-02-08 20:14:35 +0300

    feat: добавлен логотип и заглушка поиска в сайдбар

--- layout/Sidebar/Sidebar.tsx
+++ layout/Sidebar/Sidebar.tsx
@@ -2,10 +2,14 @@ import { FC } from 'react'
 import { ISidebarProps } from './Sidebar.props'
 import styles from './Sidebar.module.css'
 import { Menu } from '../Menu/Menu'
+import Logo from '../logo.svg'
+import cn from 'classnames'
 
-export const Sidebar: FC<ISidebarProps> = ({ ...props }) => {
+export const Sidebar: FC<ISidebarProps> = ({ className, ...otherProps }) => {
   return (
-    <aside {...props}>
+    <aside className={cn(className, styles.sidebar)} {...otherProps}>
+      <Logo className={styles.logo} />
+      <div>Поиск</div>
       <Menu />
     </aside>
   )
